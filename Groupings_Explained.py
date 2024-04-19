9bc39022f60369b36d3d2bc1beb5ae47941559c3
Luzi-Jung <150139805+Luzi-Jung@users.noreply.github.com>
2024-04-19 16:48:06 +0100

    Update Groupings_Explained.py

--- Groupings_Explained.py
+++ Groupings_Explained.py
@@ -1,6 +1,6 @@
 ## Explanation of Groups ##
 
-#Groups names for selecting are: 'M0', 'M1', 'M2', 'Cardiac', 'SIS', 'UMB', 'Card_M0', 'Card_M1', 'Card_M2', 'SIS_M0', 'SIS_M1', 'SIS_M2', 'UBM_M0', 'UBM_M1', 'UBM_M2'
+#Treatment group names are: 'M0', 'M1', 'M2', 'Cardiac', 'SIS', 'UMB', 'Card_M0', 'Card_M1', 'Card_M2', 'SIS_M0', 'SIS_M1', 'SIS_M2', 'UBM_M0', 'UBM_M1', 'UBM_M2'
 
 #Feature groups are: 
 #morphological_measurments = ['CellArea', 'cellbody_area', 'Cell_Elongation', 'cell_full_length', 'cell_half_width', 'Cell_length_by_area', 'Cell_width_by_area', 'NucleusArea', 'Nuc_Elongation', 'Nuc_full_length', 'Nuc_half_width', 'Nuc_Roundness', 'roundness', 'body_roundness', 'percentProtrusion', 'protrusion_extent', 'total_protrusionarea', 'mean_protrusionarea', 'number_protrusions', 'skeleton_area', 'skeleton_node_count', 'skeletonareapercent']
